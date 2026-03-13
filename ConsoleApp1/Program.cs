77739655c7c900d7dbc41ce1639e13a3b6299327
FistHead <162723928+FistHead@users.noreply.github.com>
2026-03-13 12:05:43 +0300

    Update 0

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,14 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System;
 
-class Table
+abstract class Table
 {
     public int Rows {get;}
     public int Columns {get;}
 
     public Dictionary<(int r, int c), string> Cells  = new Dictionary<(int, int), string>();
     
-
     public Table(int rows,int columns)
     {
         Rows = rows;
@@ -19,43 +19,60 @@ class Table
 
     public void Initialize()
     {
-        for (int r = 0; i < Rows; r++)
+        for (int r = 0; r < Rows; r++)
         {
-            for (int c = 0; i < Columns; c++)
+            for (int c = 0; c < Columns; c++)
             {
                 Cells[(r, c)] = "NONE";
             }
         }
     }
 
-    public void DrawAllCells()
+    public string DrawCellsData()
     {
         string data = "";
 
-        for (int r = 0; i < Rows; r++)
+        for (int r = 0; r < Rows; r++)
         {
-            for (int c = 0; i < Columns; c++)
+            for (int c = 0; c < Columns; c++)
             {
-                data += $"Row: {r}, Column: {c}, Data inside: {Cells[(r,c)]} ";
+                data += $"Row: {r}, Column: {c}, Data inside: {Cells[(r,c)]}\n";
             }
         }
+        return data;
     }
 
 }
 
-class Map
+class Block
 {
+    private int posX = 0;
+    private int posY = 0;
+}
+class Map: Table
+{  
+    private List<Block> blocks = new List<Block>();
+    public Map(int rows, int columns) : base(rows, columns)
+    {
+        
+    }
+}
+class Unit
+{
+    private int hp;
+    private int stamina;
 
 }
 
-class Block
+class Building
 {
     
 }
 
 class Button
 {
-    
+    public string command;
+    public string name;
 }
 
 class Stepper
@@ -63,11 +80,17 @@ class Stepper
     
 }
 
+class CommandManager
+{
+    public List<string> commands = new List<string> {"FOCUS"};
+}
+
 class Program 
 {
     static void Main() 
     {
-        Table testTable = new Table(3, 3);
-        Console.WriteLine(testTable.DrawAllCells());
+        // Table testTable = new Table(4, 4);
+        // Console.WriteLine(testTable.DrawCellsData());
+        Map map = new Map(4,4);
     }
 }
