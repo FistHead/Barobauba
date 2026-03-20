94635bac356e935f5f25a6a7271fa4466acff934
FistHead <162723928+FistHead@users.noreply.github.com>
2026-03-20 18:42:32 +0300

    Update 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,19 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using System;
+
 
 abstract class Table
 {
-    public int Rows {get;}
-    public int Columns {get;}
+    public int Rows { get; }
+    public int Columns { get; }
 
-    public Dictionary<(int r, int c), string> Cells  = new Dictionary<(int, int), string>();
+    public Dictionary<(int r, int c), string> Cells = new Dictionary<(int, int), string>();
     
-    public Table(int rows,int columns)
+    public Table(int rows, int columns)
     {
         Rows = rows;
         Columns = columns;
-
         Initialize();
     }
 
@@ -23,7 +23,7 @@ abstract class Table
         {
             for (int c = 0; c < Columns; c++)
             {
-                Cells[(r, c)] = "NONE";
+                Cells[(r, c)] = "[ ]";
             }
         }
     }
@@ -36,37 +36,72 @@ abstract class Table
         {
             for (int c = 0; c < Columns; c++)
             {
-                data += $"Row: {r}, Column: {c}, Data inside: {Cells[(r,c)]}\n";
+                data += $"Row: {r}, Column: {c}, Data inside: {Cells[(r, c)]}\n";
             }
         }
         return data;
     }
-
 }
 
 class Block
 {
-    private int posX = 0;
-    private int posY = 0;
+    public int posX = 0;
+    public int posY = 0;
+    public bool isCaptured = false;
 }
-class Map: Table
-{  
+
+class Map : Table
+{
     private List<Block> blocks = new List<Block>();
+
     public Map(int rows, int columns) : base(rows, columns)
     {
-        
+        GenerateWorld();
     }
+
+    public void GenerateWorld()
+    {
+        List<string> block_types = new List<string>() {"[F]","[M]","[B]","[!]","[?]"};
+        Random random = new Random();
+
+        for (int i = 0; i < Rows * Columns / 4; i++)
+        {
+            Block block = new Block();
+            block.posX = random.Next(Columns);
+            block.posY = random.Next(Rows);
+
+            blocks.Add(block);
+            
+            Cells[(block.posY, block.posX)] = "[F]";
+        }
+    }
+
+
+    public void DrawMap()
+    {
+        for (int r = 0; r < Rows; r++)
+        {
+            for (int c = 0; c < Columns; c++)
+            {
+                Console.Write(Cells[(r, c)] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public List<Block> GetBlocks() { return blocks; }
 }
+
 class Unit
 {
-    private int hp;
-    private int stamina;
-
+    public int hp;
+    public int stamina;
+    public int speed;
 }
 
 class Building
 {
-    
+
 }
 
 class Button
@@ -77,20 +112,24 @@ class Button
 
 class Stepper
 {
-    
+
 }
 
 class CommandManager
 {
-    public List<string> commands = new List<string> {"FOCUS"};
+    public List<string> commands = new List<string>() { "FOCUS" };
 }
 
-class Program 
+class Program
 {
-    static void Main() 
+    static void Main()
     {
         // Table testTable = new Table(4, 4);
         // Console.WriteLine(testTable.DrawCellsData());
-        Map map = new Map(4,4);
+
+        Map map = new Map(3,3);
+        map.DrawMap();
+
+
     }
 }
